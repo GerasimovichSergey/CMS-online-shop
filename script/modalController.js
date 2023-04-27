6e754a00518cab1483736cdf1087f72fd2e73772
Gerasimovich Sergey <wmmoee@gmail.com>
2023-04-27 08:57:54 +0300

    add modal and preview controllers

---
+++ script/modalController.js
@@ -0,0 +1,28 @@
+import { form } from './elems.js';
+
+const openModal = (modal, classOpen) => {
+  modal.classList.add(classOpen);
+}
+
+const closeModal = (modal, classOpen) => {
+  modal.classList.remove(classOpen);
+};
+
+const clearFormData = (form) => {
+  form.reset();
+};
+
+export const modalController = (obj) => {
+  const { modal, modalBtn, classOpen, classClose } = obj;
+
+  modalBtn.addEventListener('click', () => {
+    openModal(modal, classOpen);
+  });
+
+  modal.addEventListener('click', (event) => {
+    if (event.target === modal || event.target.classList.contains(classClose)) {
+      closeModal(modal, classOpen);
+      clearFormData(form);
+    }
+  });
+};
---
+++ script/utils.js
@@ -0,0 +1,15 @@
+export const toBase64 = (file) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.addEventListener('loadend', () => {
+      resolve(reader.result);
+    });
+
+    reader.addEventListener('error', (error) => {
+      reject(error);
+    });
+
+    reader.readAsDataURL(file);
+  })
+};
